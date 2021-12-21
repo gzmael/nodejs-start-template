deedd8b005034533507179901407d7bf48e476b4
Jezmael Basilio <jezmaelbasilio@gmail.com>
2021-12-20 21:09:09 -0300

    test(scopo): adicionando o scopo para teste de integracao

---
+++ src/modules/users/services/authenticateUser/AuthenticateUserController.test.ts
@@ -0,0 +1,9 @@
+describe('Authenticate User Controller', () => {
+  it('Should be able authenticate a user', async () => {
+    expect({
+      message: 'Hello World',
+    }).toEqual({
+      message: 'Hello World',
+    });
+  });
+});
